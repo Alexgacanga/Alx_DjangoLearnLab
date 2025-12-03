77647e128a8ac27f787124b9a5d6c4e7a3c53f0e
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-12-03 11:18:26 +0300

    Update

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+
+
 class SearchForm(forms.Form):
     query = forms.CharField(required=False, max_length=100)

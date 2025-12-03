bd45afb8bfdde40572af1175eca8e2c882cae671
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-12-03 11:21:38 +0300

    Update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .forms import SearchForm
+from .forms import ExampleForm
 
 
 @permission_required('content.can_view', raise_exception=True)
